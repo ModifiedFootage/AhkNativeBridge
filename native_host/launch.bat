678120182320116593c4a37182a0f25c9ab6369f
ModifiedFootage <ut_tommy@hotmail.com>
2025-04-22 10:06:59 +0930

    Working solutin established.

--- native_host/launch.bat
+++ native_host/launch.bat
@@ -8,9 +8,8 @@ set VENV_PYTHON="%~dp0venv\Scripts\python.exe"
 :: Path to the python script relative to this batch file
 set SCRIPT_PATH="%~dp0native_bridge.py"
 
-:: Define log file paths relative to this batch file's location
-set STDOUT_LOG="%~dp0native_bridge_stdout.log"
+:: Define ONLY the stderr log file path
 set STDERR_LOG="%~dp0native_bridge_stderr.log"
 
-:: Execute the script using the venv's python, redirecting output
-%VENV_PYTHON% %SCRIPT_PATH% > %STDOUT_LOG% 2> %STDERR_LOG%
+:: Standard output (>) is LEFT ALONE so the browser can read it.
+%VENV_PYTHON% %SCRIPT_PATH% 2> %STDERR_LOG%
